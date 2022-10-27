5b0219586a2c523988196a23f7825c42c8f354fe
Jatin shankar <shankarjatin1005@gmail.com>
2022-10-27 21:22:29 +0530

    subtraction section minor changes

--- app.js
+++ app.js
@@ -27,7 +27,7 @@ app.get("/calculate/:operation",(req,res) =>
   const result = num1 + num2
   res.send(result + "")
   }
-  if else(opr == "subtract")
+  else if (opr == "subtract")
   {
   const num1 = Number(req.query.num1)
   const num2 = Number(req.query.num2)
